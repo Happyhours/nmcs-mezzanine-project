15d58be04e5edaabaf949df07982c7d84323cb95
jonathan <jonathan@ubuntu.(none)>
2014-07-08 20:05:21 +0200

    Added simple models and admin that work

--- nmcs_project/garis_theme/admin.py
+++ nmcs_project/garis_theme/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from garis_theme.models import (
+    HomePage,
+)
+
+class HomePageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['heading','subheading']}),
+        #('jallo',  {'fields': ['comment']}),
+    ]
+
+admin.site.register(HomePage, HomePageAdmin)
--- nmcs_project/garis_theme/models.py
+++ nmcs_project/garis_theme/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from mezzanine.core.fields import FileField, RichTextField
+from mezzanine.core.models import RichText, Orderable, Slugged
+from mezzanine.pages.models import Page
+from mezzanine.utils.models import upload_to
 
 # Create your models here.
+
+
+class HomePage(Page, RichText):
+    '''
+    A page representing the format of the home page
+    '''
+    heading = models.CharField(max_length=200,
+        help_text="Heading for HomePage")
+    subheading = models.CharField(max_length=200, blank=True,
+        help_text="Optional subheading for HomePage")
+
+    def __str__(self):
+        return self.heading
+
+# class Paragraph(Orderable):
+#     '''
+#     A paragraph for paragraphs in HomePage
+#     '''
+#     homepage = models.ForeignKey(HomePage, related_name="slides")
+#     text = models.TextField()
