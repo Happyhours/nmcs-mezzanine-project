a18e25ae8d18926d54bcd9aef98d49c2a742f57b
Jonathan Doherty <jonathan@Jonathans-MacBook-Pro.local>
2014-08-01 14:09:58 +0200

    Fixing more contact page bugs

--- garis_theme/page_processors.py
+++ garis_theme/page_processors.py
@@ -30,35 +30,35 @@ class ContactForm(forms.Form):
     email = forms.EmailField()
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea())
-    # captcha = MathCaptchaField(
-    #     error_messages={
-    #         'invalid': 'Fel svar var god försök igen.',
-    #         'invalid_number': 'Var god ange heltal endast.',
-    #         'required': 'Fyll i Captcha fältet', 
-    #     },
-    #     widget=MathCaptchaWidget(
-    #         attrs={'class': 'form-control'},
-    #         question_tmpl = ('Vad är %(num1)i %(operator)s %(num2)i?')
-    #     )
-    # )
-
-    helper = FormHelper()
-    helper.form_tag = False
-    helper.form_show_labels = False
-    helper.layout = Layout(
-        Div(Field('name', placeholder="Namn"), css_class="row"),
-        Div(Field('email', placeholder="Email"), css_class="row"),
-        Div(Field('subject', placeholder="Rubrik"), css_class="row"),
-        Div(Field('message', rows=10), css_class="row"),
-        Div(Field('captcha'), css_class="row"),
-        Div(
-            FormActions(
-                Submit('Skicka', 'submit', css_class='btn btn-dark big'),
-                Reset('Ångra', 'reset', css_class='btn btn-dark big')
-            ),
-            css_class="row",
-        ),
+    captcha = MathCaptchaField(
+        error_messages={
+            'invalid': 'Fel svar var god försök igen.',
+            'invalid_number': 'Var god ange heltal endast.',
+            'required': 'Fyll i Captcha fältet', 
+        },
+        widget=MathCaptchaWidget(
+            attrs={'class': 'form-control'},
+            question_tmpl = ('Vad är %(num1)i %(operator)s %(num2)i?')
+        )
     )
+
+    # helper = FormHelper()
+    # helper.form_tag = False
+    # helper.form_show_labels = False
+    # helper.layout = Layout(
+    #     Div(Field('name', placeholder="Namn"), css_class="row"),
+    #     Div(Field('email', placeholder="Email"), css_class="row"),
+    #     Div(Field('subject', placeholder="Rubrik"), css_class="row"),
+    #     Div(Field('message', rows=10), css_class="row"),
+    #     Div(Field('captcha'), css_class="row"),
+    #     Div(
+    #         FormActions(
+    #             Submit('Skicka', 'submit', css_class='btn btn-dark big'),
+    #             Reset('Ångra', 'reset', css_class='btn btn-dark big')
+    #         ),
+    #         css_class="row",
+    #     ),
+    # )
 
 
 # @processor_for(ContactPage)
